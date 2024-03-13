71fd4c404089a480ac0a6e2ffa89ed2630a2366d
Igor Storchevoy <hellsgor@gmail.com>
2024-03-13 15:28:06 +0400

    feat(main.js): added ability to close a modal by clicking on backdrop

--- frontend/main.js
+++ frontend/main.js
@@ -4,11 +4,20 @@ import { initModals } from '@/js/Modal.js';
 
 const clientsTable = new ClientsTable(document.getElementById('table-body'));
 const addClientButton = document.getElementById('add-client-button');
+const backdrop = document.getElementById('backdrop');
+const modals = initModals();
 
 await clientsTable.renderClients();
 
-const modals = initModals();
-
 addClientButton.addEventListener('click', () => {
   modals['client'].showModal();
+});
+
+backdrop.addEventListener('click', () => {
+  Object.keys(modals).forEach((modalName) => {
+    if (modals[modalName].modal.classList.contains('modal_hidden')) {
+      return;
+    }
+    modals[modalName].closeModal();
+  });
 });
