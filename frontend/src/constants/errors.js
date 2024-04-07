faeaa7b33d5cb8b60018b5d53578d5d5f4ab766d
Igor Storchevoy <hellsgor@gmail.com>
2024-04-07 14:04:00 +0400

    fix(Errors): fixed line length

--- frontend/src/constants/errors.js
+++ frontend/src/constants/errors.js
@@ -3,5 +3,9 @@ export const ERRORS = {
     FOR_ERRORS_CONTROL_NAME: 'data-for-errors-control-name',
   },
   EF001: (input) =>
-    `Поле ${input.hasAttribute(ERRORS.ATTRS.FOR_ERRORS_CONTROL_NAME) ? `"${input.getAttribute(ERRORS.ATTRS.FOR_ERRORS_CONTROL_NAME)}" ` : ''}не может быть пустым`,
+    `Поле ${
+      input.hasAttribute(ERRORS.ATTRS.FOR_ERRORS_CONTROL_NAME)
+        ? `"${input.getAttribute(ERRORS.ATTRS.FOR_ERRORS_CONTROL_NAME)}" `
+        : ''
+    }не может быть пустым`,
 };
