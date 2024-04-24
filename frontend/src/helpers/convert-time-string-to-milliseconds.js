132a65f632d3afb737033204bb42a72fd3dbb381
Igor Storchevoy <hellsgor@gmail.com>
2024-04-25 01:00:52 +0400

    doc(convert-time-string-to-milliseconds.js): добавлена документация

--- frontend/src/helpers/convert-time-string-to-milliseconds.js
+++ frontend/src/helpers/convert-time-string-to-milliseconds.js
@@ -1,3 +1,8 @@
+/**
+ * Преобразует строку времени в миллисекунды.
+ * @param {string} timeString - Строка времени в формате CSS, например, '0.5s' или '100ms'.
+ * @returns {number|null} - Значение времени в миллисекундах или null, если строка не может быть преобразована.
+ */
 export function convertTimeStringToMilliseconds(timeString) {
   const multiplier = timeString.includes('s') ? 1000 : 1;
   const numericValue = parseFloat(timeString);
