92cbf0a785d361cb4a5ed849bf4124c4808fc7fd
Igor Storchevoy <hellsgor@gmail.com>
2024-04-24 16:56:33 +0400

    feat(main.js): изменены классы записанные в виде строк на значения из MODALS

--- frontend/main.js
+++ frontend/main.js
@@ -2,6 +2,7 @@ import '@styles/index.scss';
 import { initModals } from '@/js/Modal.js';
 import { autoInitForms } from '@/js/Form.js';
 import { clientsTable } from '@/js/ClientsTable.js';
+import { MODALS } from '@/constants/modals.js';
 
 const addClientButton = document.getElementById('add-client-button');
 const backdrop = document.getElementById('backdrop');
@@ -10,12 +11,16 @@ const modals = initModals();
 await clientsTable.initClients(modals).then(() => clientsTable.renderClients());
 
 addClientButton.addEventListener('click', () => {
-  modals['new-client'].showModal();
+  modals[MODALS.TEMPLATES.NEW_CLIENT].showModal();
 });
 
 backdrop.addEventListener('click', () => {
   Object.keys(modals).forEach((modalName) => {
-    if (modals[modalName].modal.classList.contains('modal_hidden')) {
+    if (
+      modals[modalName].modal.classList.contains(
+        `${MODALS.CLASS_NAMES.MODAL_CLASS_NAME}${MODALS.MODIFIERS.HIDDEN}`,
+      )
+    ) {
       return;
     }
     modals[modalName].closeModal();
