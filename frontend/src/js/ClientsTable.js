2b8ef2e2bdd03d40041eb80fd1c3fa50d6195c80
Igor Storchevoy <hellsgor@gmail.com>
2024-04-26 00:41:24 +0400

    doc(ClientsTable): добавлена документация

--- frontend/src/js/ClientsTable.js
+++ frontend/src/js/ClientsTable.js
@@ -2,17 +2,32 @@ import clientsApi from '@api/clients-api.js';
 import { Client } from '@/js/Client.js';
 import { sortContactsTypes } from '@/helpers/sort-contacts-types.js';
 
+/**
+ * @description Класс для управления таблицей клиентов.
+ * @param {Array || null} clients Список клиентов.
+ * @param {HTMLTableSectionElement || null} tBody - Элемент tbody таблицы, в который будут добавляться клиенты.
+ * @param {Object || null} modals Модальные окна, используемые для клиентов.
+ * @param {Array || null} sortedContactsTypes Отсортированные типы контактов.
+ */
 class ClientsTable {
   clients = null;
   tBody = null;
   modals = null;
-  sortedContactsTypes = null;
+  sortedContactsTypes = sortContactsTypes();
 
+  /**
+   * @description Создает экземпляр таблицы клиентов.
+   * @param {HTMLTableSectionElement} tBody - Элемент tbody таблицы, в который будут добавляться клиенты.
+   */
   constructor(tBody) {
     this.tBody = tBody || null;
-    this.sortedContactsTypes = sortContactsTypes() || null;
   }
 
+  /**
+   * @description Инициализирует список клиентов.
+   * @param {Object} [modals=null] - Модальные окна, используемые для клиентов.
+   * @returns {Promise<void>} - Промис, который разрешается после загрузки клиентов.
+   */
   async initClients(modals = null) {
     if (modals) {
       this.modals = modals;
@@ -29,6 +44,9 @@ class ClientsTable {
     }
   }
 
+  /**
+   * @description Рендерит список клиентов в таблицу.
+   */
   renderClients() {
     if (!this.tBody) {
       console.error('Элемент таблицы не найден');
@@ -41,16 +59,28 @@ class ClientsTable {
     });
   }
 
+  /**
+   * @description Рендерит клиента в таблицу.
+   * @param {Client} client - Клиент для рендеринга.
+   */
   renderClient(client) {
     this.tBody.insertAdjacentElement('beforeend', client.getClientRow());
   }
 
+  /**
+   * @description Добавляет нового клиента в список.
+   * @param {Object} clientData - Данные нового клиента.
+   */
   addClient(clientData) {
     this.clients.push(
       new Client(clientData, this.modals, this.sortedContactsTypes),
     );
   }
 
+  /**
+   * @description Удаляет клиента из списка.
+   * @param {string || null} clientID - Идентификатор клиента для удаления.
+   */
   removeClient(clientID) {
     const indexToRemove = this.clients.findIndex(
       (client) => client.clientData.id === clientID,
@@ -61,11 +91,12 @@ class ClientsTable {
     }
   }
 
+  /**
+   * @description Очищает таблицу.
+   */
   clearTable() {
     this.tBody.innerHTML = '';
   }
-
-  // TODO: написать документацию в ClientsTable
 }
 
 export const clientsTable = new ClientsTable(
