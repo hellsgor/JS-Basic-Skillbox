6ac5961d457188ad8ca90067d6c3adde645a1758
Igor Storchevoy <hellsgor@gmail.com>
2024-04-25 21:11:38 +0400

    feat(CONTACTS) добавлены свойства к свойствам TYPES

--- frontend/src/constants/contacts.js
+++ frontend/src/constants/contacts.js
@@ -1,15 +1,47 @@
 export const CONTACTS = {
   TYPES: {
-    PHONE_NUMBER: { text: 'Телефон', value: 'phone-number', inputType: 'tel' },
+    EMAIL: {
+      text: 'Email',
+      value: 'email',
+      inputType: 'email',
+      sortIndex: 3,
+      modifier: 'email',
+    },
+    PHONE_NUMBER: {
+      text: 'Телефон',
+      value: 'phone-number',
+      inputType: 'tel',
+      sortIndex: 1,
+      modifier: 'phone',
+    },
+    VK: {
+      text: 'Vk',
+      value: 'vk',
+      inputType: 'url',
+      sortIndex: 4,
+      modifier: 'vk',
+    },
+    TWITTER: {
+      text: 'Twitter',
+      value: 'twitter',
+      inputType: 'url',
+      sortIndex: null,
+      modifier: '',
+    },
     ADDITIONAL_PHONE_NUMBER: {
       text: 'Доп. телефон',
       value: 'additional-phone-number',
       inputType: 'tel',
+      sortIndex: 2,
+      modifier: 'phone',
     },
-    EMAIL: { text: 'Email', value: 'email', inputType: 'email' },
-    VK: { text: 'Vk', value: 'vk', inputType: 'url' },
-    FACEBOOK: { text: 'Facebook', value: 'facebook', inputType: 'url' },
-    TWITTER: { text: 'Twitter', value: 'twitter', inputType: 'url' },
+    FACEBOOK: {
+      text: 'Facebook',
+      value: 'facebook',
+      inputType: 'url',
+      sortIndex: 5,
+      modifier: 'facebook',
+    },
   },
   CLASS_NAMES: {
     input: 'contact-control__input',
