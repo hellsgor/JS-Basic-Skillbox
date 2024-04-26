dd1410672dfe828b7e51fc8083f490cfe562a0e7
Igor Storchevoy <hellsgor@gmail.com>
2024-04-26 17:32:12 +0400

    fix(Form): рефакторинг processingResponseErrors

--- frontend/src/js/Form.js
+++ frontend/src/js/Form.js
@@ -388,71 +388,114 @@ export class Form {
   }
 
   /**
-   * @description - Обрабатывает ошибки из ответа сервера
-   * @param response - объект ответа сервера (Axios)
-   * */
+   * @description Обрабатывает ошибки из ответа сервера.
+   * @param {Object} response - Объект ответа сервера (Axios).
+   */
   processingResponseErrors(response) {
+    if (this.handleNetworkError(response)) {
+      return;
+    }
+
+    if (this.handleValidationErrors(response)) {
+      return;
+    }
+  }
+
+  /**
+   * @description Обрабатывает ошибки сети и статус 404.
+   * @param {Object} response - Объект ответа сервера (Axios).
+   * @returns {boolean} Возвращает true, если ошибка сети или статус 404, иначе false.
+   */
+  handleNetworkError(response) {
     if (
       response.error.code === 'ERR_NETWORK' ||
       response.error.response.status === 404 ||
       response.error.response.status >= 500
     ) {
       this.showError(ERRORS.EF003());
-      return;
+      return true;
     }
+    return false;
+  }
 
+  /**
+   * @description Обрабатывает ошибки валидации.
+   * @param {Object} response - Объект ответа сервера (Axios).
+   * @returns {boolean} Возвращает true, если есть ошибки валидации, иначе false.
+   */
+  handleValidationErrors(response) {
     if (response.error.response.status === 422) {
       response.error.response.data.errors.forEach((error) => {
         if (error.field !== FORMS.CLIENT_OBJECT_CONTACTS_PROPERTY_NAME) {
-          this.invalidate(
-            Array.from(this.controls).find(
-              (control) => control.name === error.field,
-            ),
-            { text: error.message, code: null },
-          );
+          this.handleControlError(error);
         } else {
-          this.controls.forEach((control) => {
-            if (
-              control.closest(`.${this.classNames.modalContact}`) &&
-              !control.value.trim()
-            ) {
-              ++this.errorsCounter;
-              this.addErrorStyle(control, false);
-              control.addEventListener('input', (event) => {
-                const errorText = error.message;
-                let isSomeContactsWithError = false;
-
-                this.removeError(event, false);
-
-                this.controls.forEach((control) => {
-                  if (
-                    control.closest(`.${this.classNames.modalContact}`) &&
-                    control
-                      .closest(`.${this.classNames.modalContact}`)
-                      .classList.contains(
-                        `${this.classNames.modalContactWithError}`,
-                      )
-                  ) {
-                    isSomeContactsWithError = true;
-                  }
-                });
-                if (!isSomeContactsWithError) {
-                  const contactsErrorElement = Array.from(
-                    this.errorsWrapper.querySelectorAll(
-                      `.${this.classNames.modalError}`,
-                    ),
-                  ).find(
-                    (errorElement) => errorElement.textContent === errorText,
-                  );
-
-                  contactsErrorElement && contactsErrorElement.remove();
-                }
-              });
-            }
-          });
-          this.showError(error.message);
+          this.handleContactError(error);
         }
       });
+      return true;
+    }
+    return false;
+  }
+
+  /**
+   * @description Обрабатывает ошибки контролов формы.
+   * @param {Object} error - Объект с информацией об ошибке.
+   */
+  handleControlError(error) {
+    const control = Array.from(this.controls).find(
+      (control) => control.name === error.field,
+    );
+    if (control) {
+      this.invalidate(control, { text: error.message });
+    }
+  }
+
+  /**
+   * @description Обрабатывает ошибки контактов формы.
+   * @param {Object} error - Объект с информацией об ошибке.
+   */
+  handleContactError(error) {
+    this.controls.forEach((control) => {
+      if (
+        control.closest(`.${this.classNames.modalContact}`) &&
+        !control.value.trim()
+      ) {
+        this.handleEmptyContact(control, error.message);
+      }
+    });
+  }
+
+  /**
+   * @description Обрабатывает ошибки пустых контактов формы.
+   * @param {HTMLInputElement} control - Контрол контакта, у которого значение пустое.
+   * @param {string} errorMessage - Текст ошибки.
+   */
+  handleEmptyContact(control, errorMessage) {
+    ++this.errorsCounter;
+    this.addErrorStyle(control, false);
+    control.addEventListener('input', () =>
+      this.removeEmptyContactError(errorMessage),
+    );
+    this.showError(errorMessage);
+  }
+
+  /**
+   * @description Удаляет ошибки пустых контактов формы.
+   * @param {string} errorMessage - Текст ошибки.
+   */
+  removeEmptyContactError(errorMessage) {
+    const isSomeContactsWithError = Array.from(this.controls).some(
+      (control) =>
+        control.closest(`.${this.classNames.modalContact}`) &&
+        control
+          .closest(`.${this.classNames.modalContact}`)
+          .classList.contains(`${this.classNames.modalContactWithError}`),
+    );
+    if (!isSomeContactsWithError) {
+      const contactsErrorElement = Array.from(
+        this.errorsWrapper.querySelectorAll(`.${this.classNames.modalError}`),
+      ).find((errorElement) => errorElement.textContent === errorMessage);
+      contactsErrorElement && contactsErrorElement.remove();
     }
   }
 }
