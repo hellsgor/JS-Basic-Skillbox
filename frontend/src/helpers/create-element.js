a1cb18e32aecbe56fea5a82d86c822bdf8b95449
Igor Storchevoy <hellsgor@gmail.com>
2024-04-30 16:45:48 +0400

    fix(create-element): деструктурированы параметры функции createElement

--- frontend/src/helpers/create-element.js
+++ frontend/src/helpers/create-element.js
@@ -1,35 +1,43 @@
 export function createElement(props) {
   const element = document.createElement(props.tag);
 
-  if (Array.isArray(props.classes)) {
-    props.classes.forEach((classesItem) => {
+export function createElement({
+  tag,
+  classes,
+  text,
+  html,
+  attributes,
+  callback,
+  event,
+}) {
+  const element = document.createElement(tag);
+
+  if (Array.isArray(classes)) {
+    classes.forEach((classesItem) => {
       element.classList.add(classesItem);
     });
   }
 
-  if (typeof props.classes === 'string') {
-    element.className = props.classes;
+  if (typeof classes === 'string') {
+    element.className = classes;
   }
 
-  if (props.text) {
-    element.innerText = props.text;
+  if (text) {
+    element.innerText = text;
   }
 
-  if (props.html) {
-    element.innerHTML = props.html;
+  if (html) {
+    element.innerHTML = html;
   }
 
-  if (props.attributes && Array.isArray(props.attributes)) {
-    props.attributes.forEach((attr) => {
+  if (attributes && Array.isArray(attributes)) {
+    attributes.forEach((attr) => {
       element.setAttribute(attr.name, attr.value);
     });
   }
 
-  if (props.callback) {
-    element.addEventListener(
-      props.event ? props.event : 'click',
-      props.callback,
-    );
+  if (callback) {
+    element.addEventListener(event ? event : 'click', callback);
   }
 
   return element;
