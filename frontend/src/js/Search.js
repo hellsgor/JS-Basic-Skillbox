5349c08f1aebeffc1131b6ea2b3b525c94c1b2cf
Igor Storchevoy <hellsgor@gmail.com>
2024-05-20 19:32:51 +0400

    feat(Search): реализована обработка отсутствия результатов поиска

--- frontend/src/js/Search.js
+++ frontend/src/js/Search.js
@@ -1,33 +1,57 @@
 import { debouncer } from '@/js/Debouncer.js';
 import { clientsTable } from '@/js/ClientsTable.js';
 import clientsApi from '@api/Clients-api.js';
+import { SearchNoResults } from '@/js/SearchNoResults.js';
 
 class Search {
-  searchComponent = null;
-  control = null;
+  $searchComponent = null;
+  $control = null;
+
+  searchNoResult = null;
 
   classNames = {
     control: 'liner-control__input',
   };
-  controlInputHandler = debouncer.debounce(this.getSearchResult, 300);
+
+  timeout = 300;
+  controlInputHandler = debouncer.debounce(
+    this.controlInputHandlerMethod.bind(this),
+    this.timeout,
+  );
 
   constructor(searchComponent) {
-    this.searchComponent = searchComponent || null;
+    this.$searchComponent = searchComponent || null;
   }
 
   init() {
     this.getElements();
+    this.searchNoResult = new SearchNoResults();
     this.addListeners();
   }
 
   getElements() {
-    this.control = this.searchComponent.querySelector(
+    this.$control = this.$searchComponent.querySelector(
       `.${this.classNames.control}`,
     );
   }
 
   addListeners() {
-    this.control.addEventListener('input', this.controlInputHandler);
+    this.$control.addEventListener('input', this.controlInputHandler);
+  }
+
+  controlInputHandlerMethod({ target }) {
+    if (target.value) {
+      this.getSearchResult({ target }).then(() => {
+        if (!clientsTable.clients.length) {
+          this.noResultsHandler(target.value);
+        }
+      });
+    } else {
+      this.searchNoResult.hide();
+      clientsTable.initClients(null, null).then(() => {
+        clientsTable.renderClients();
+      });
+    }
   }
 
   async getSearchResult({ target }) {
@@ -35,7 +59,19 @@ class Search {
       .initClients(null, clientsApi.searchClients, {
         searchString: target.value,
       })
-      .then(() => clientsTable.renderClients());
+      .then(() => {
+        if (clientsTable.clients.length) {
+          clientsTable.renderClients();
+        }
+      });
+  }
+
+  noResultsHandler(query) {
+    clientsTable.hidePreloader();
+    this.searchNoResult.show(
+      `Клиенты по запросу "${query}" не найдены. Попробуйте другой запрос`,
+      'Сбросить поиск',
+    );
   }
 }
 
