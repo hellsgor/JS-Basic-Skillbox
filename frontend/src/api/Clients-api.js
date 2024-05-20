87b724d69d7739bad3abb98e8e638a9935037b58
Igor Storchevoy <hellsgor@gmail.com>
2024-05-20 21:24:37 +0400

    doc(ClientsApi): добавлена документация

--- frontend/src/api/Clients-api.js
+++ frontend/src/api/Clients-api.js
@@ -1,14 +1,33 @@
 import axios from 'axios';
 
+/**
+ * Класс для взаимодействия с API клиентов.
+ */
 class ClientsApi {
-  resource = 'clients';
-
+  /**
+   * Конструктор класса ClientsApi.
+   * Создает экземпляр axios с базовым URL.
+   */
   constructor() {
+    /**
+     * Ресурс API для клиентов.
+     * @type {string}
+     */
+    this.resource = 'clients';
+
+    /**
+     * Экземпляр axios для HTTP-запросов.
+     * @type {axios.AxiosInstance}
+     */
     this.api = axios.create({
       baseURL: 'http://localhost:3000/api/',
     });
   }
 
+  /**
+   * Получение списка клиентов.
+   * @returns {Promise<Object[]>} Массив клиентов.
+   */
   async getClients() {
     try {
       const response = await this.api.get(this.resource);
@@ -18,6 +37,12 @@ class ClientsApi {
     }
   }
 
+  /**
+   * Получение информации о клиенте по его ID.
+   * @param {Object} props - Параметры запроса.
+   * @param {number} props.id - ID клиента.
+   * @returns {Promise<Object>} Данные клиента.
+   */
   async getClient(props) {
     try {
       const response = await this.api.get(`${this.resource}/${props.id}`);
@@ -27,6 +52,12 @@ class ClientsApi {
     }
   }
 
+  /**
+   * Удаление клиента по его ID.
+   * @param {Object} props - Параметры запроса.
+   * @param {number} props.id - ID клиента.
+   * @returns {Promise<axios.AxiosResponse>} Ответ сервера.
+   */
   async deleteClient(props) {
     try {
       const response = await this.api.delete(`${this.resource}/${props.id}`);
@@ -37,6 +68,13 @@ class ClientsApi {
     }
   }
 
+  /**
+   * Редактирование данных клиента.
+   * @param {Object} props - Параметры запроса.
+   * @param {number} props.id - ID клиента.
+   * @param {Object} props.data - Новые данные клиента.
+   * @returns {Promise<axios.AxiosResponse>} Ответ сервера.
+   */
   async editClient(props) {
     try {
       const response = await this.api.patch(
@@ -50,6 +88,12 @@ class ClientsApi {
     }
   }
 
+  /**
+   * Добавление нового клиента.
+   * @param {Object} props - Параметры запроса.
+   * @param {Object} props.data - Данные нового клиента.
+   * @returns {Promise<axios.AxiosResponse>} Ответ сервера.
+   */
   async addClient(props) {
     try {
       const response = await this.api.post(this.resource, props.data);
@@ -60,6 +104,12 @@ class ClientsApi {
     }
   }
 
+  /**
+   * Поиск клиентов по строке запроса.
+   * @param {Object} props - Параметры запроса.
+   * @param {string} props.searchString - Строка поиска.
+   * @returns {Promise<Object[]>} Найденные клиенты.
+   */
   async searchClients({ searchString }) {
     try {
       const response = await this.api.get(this.resource, {
@@ -72,6 +122,10 @@ class ClientsApi {
     }
   }
 
+  /**
+   * Обработка ошибок HTTP-запросов.
+   * @param {Error} error - Ошибка запроса.
+   */
   handleError(error) {
     console.error(`Невозможно выполнить операцию из-за ошибки: ${error}`);
   }
