db47d0d2b0ec0df0c93cd8de28464488ac2f08c6
Igor Storchevoy <hellsgor@gmail.com>
2024-05-20 21:31:41 +0400

    doc(addBackdropClickListener): добавлена документация

--- frontend/src/helpers/add-backdrop-click-listener.js
+++ frontend/src/helpers/add-backdrop-click-listener.js
@@ -1,5 +1,13 @@
 import { MODALS } from '@/constants/modals.js';
 
+/**
+ * Добавляет обработчик клика на элемент backdrop, который закрывает все открытые модальные окна.
+ *
+ * @param {Object} modals - Объект, содержащий модальные окна.
+ * @param {Object} modals.modalName - Объект, описывающий конкретное модальное окно.
+ * @param {HTMLElement} modals.modalName.modal - HTML элемент модального окна.
+ * @param {Function} modals.modalName.closeModal - Метод для закрытия модального окна.
+ */
 export function addBackdropClickListener(modals) {
   document.getElementById('backdrop').addEventListener('click', () => {
     Object.keys(modals).forEach((modalName) => {
